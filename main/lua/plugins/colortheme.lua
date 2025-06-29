34c0c30d7c790ef5064162f846144355d4c7eb73
IsakOswald <isak.oswald.cs@gmail.com>
2025-06-29 18:17:33 +1000

    Update color

--- main/lua/plugins/colortheme.lua
+++ main/lua/plugins/colortheme.lua
@@ -22,14 +22,11 @@
 --}
 
 return {
-    "owickstrom/vim-colors-paramount",
-    lazy = false,
-    priority = 1000,
+    "shaunsingh/nord.nvim",
+    lazy = false, -- Load the plugin immediately
+    priority = 1000, -- Ensure it's loaded first
     config = function()
-        vim.opt.termguicolors = true
-        vim.cmd("colorscheme paramount")
-
-        -- Override the background to a dark grey (adjust hex as you like)
-        vim.api.nvim_set_hl(0, "Normal", { bg = "#1e1e1e" })
+        -- Set the color scheme to Nord
+        vim.cmd("colorscheme nord")
     end,
 }
