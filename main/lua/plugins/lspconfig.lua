c0e2eda4e7b4226109fd5b66659ee3ce3a3846b6
Isak Kurt Oswald <isak.oswald.cs@gmail.com>
2025-07-23 02:02:56 +1000

    Comment out LSP argument hints

--- main/lua/plugins/lspconfig.lua
+++ main/lua/plugins/lspconfig.lua
@@ -52,6 +52,9 @@ return {
             callback = function(event)
                 -- NOTE: Remember that Lua is a real programming language, and as such it is possible
                 -- to define small helper and utility functions so you don't have to repeat yourself.
+                    
+                --[[This is used for our lsp-argument-hints plugin.]]
+                --require("lsp_signature").on_attach({}, event.buf)
 
                 -- In this case, we create a function that lets us more easily define mappings specific
                 -- for LSP related items. It sets the mode, buffer and description for us each time.
