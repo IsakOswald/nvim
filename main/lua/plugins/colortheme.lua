5abb3b20598ae20ef5a2f4d9aa11b4f8513ae732
Isak Kurt Oswald <isak.oswald.cs@gmail.com>
2025-07-23 01:53:09 +1000

    Update colortheme.lua

--- main/lua/plugins/colortheme.lua
+++ main/lua/plugins/colortheme.lua
@@ -22,11 +22,30 @@
 --}
 
 return {
-    "shaunsingh/nord.nvim",
-    lazy = false, -- Load the plugin immediately
-    priority = 1000, -- Ensure it's loaded first
+    "marko-cerovac/material.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-        -- Set the color scheme to Nord
-        vim.cmd("colorscheme nord")
+        -- Set Material variant
+        vim.g.material_style = "palenight"
+
+        -- Apply font styling preferences
+        require("material").setup({
+            italics = {
+                comments = true,
+                keywords = false,
+                functions = false,
+                strings = false,
+                variables = true,
+            },
+            disable = {
+                background = false,
+                term_colors = false,
+                eob_lines = true,
+            },
+        })
+
+        -- Load the colorscheme
+        vim.cmd("colorscheme material")
     end,
 }
